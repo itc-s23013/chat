94a1057cc20dd5c2e6a8e0269b0ac97f3b95edcb
苦しみ悶える <s23013@std.it-college.ac.jp>
2024-02-24 10:03:23 +0000

    naosita

--- pages/Recipe.js
+++ pages/Recipe.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 const Recipe = props => {
   console.log(props)
+  // props.ingredients が存在しない場合に空配列を代入
+  const ingredients = props.ingredients || []
+
   return (
     <div>
       <h1>{props.title}</h1>
       <ol>
-        {props.ingredients.map(ingredient => (
+        {ingredients.map(ingredient => (
           <li key={ingredient.id}>{ingredient.text}</li>
         ))}
       </ol>
