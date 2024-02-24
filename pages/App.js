ed29ff99682e11a83c7944203bac2529160c7c74
苦しみ悶える <s23013@std.it-college.ac.jp>
2024-02-24 05:58:28 +0000

    APP取り込み終わり

---
+++ pages/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// components
+import Edamam from 'Edamam'
+
+function App () {
+  return (
+    <>
+      <Edamam />
+    </>
+  )
+}
+
+export default App
---
+++ pages/index.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react'
+
+// components
+import Recipe from './Recipe'
+
+const Edamam = () => {
+  // keyの設置
+  const APPLICATION_ID = '43df669e'
+  const APPLICATION_KEY = '9c55d3e0def0a4217fc88980063d2985'
+
+  // stateの作成
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('banana', 'orange')
+  const [recipes, setRecipes] = useState([])
+
+  // APIの取得
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APPLICATION_ID}&app_key=${APPLICATION_KEY}`
+    )
+    const data = await response.json()
+    setRecipes(data.hits)
+    console.log(data.hits)
+  }
+
+  const getSearch = e => {
+    e.preventDefault()
+    setQuery(search)
+    setSearch('')
+  }
+
+  // 入力した時にsearchの更新
+  const updateSearch = e => {
+    setSearch(e.target.value)
+  }
+
+  useEffect(() => {
+    getRecipes()
+  }, [query])
+
+  return (
+    <>
+      <>下に入力</>
+      <form onSubmit={getSearch}>
+        <input type='text' value={search} onChange={updateSearch} />
+        <button type='submit'>検索</button>
+      </form>
+      <div>
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.title}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default Edamam
